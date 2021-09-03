793adffeac69b71cd40f7464ae7ea9e92c59c62d
Mason Fleming <mgfleming@unomaha.edu>
2021-09-03 16:26:47 -0500

    hook up data in editor

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,9 +1,10 @@
+using Fleming.Assets.ScriptableObjects;
 using UnityEngine;
 
 namespace Fleming.Assets.Scripts
 {
     public class Card : MonoBehaviour
     {
-        
+        public CardData cardData;
     }
 }
